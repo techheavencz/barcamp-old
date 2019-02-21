ac4e601ee990de86b6a4ceea61307f73c900c491
Vojtěch Pšenák <psenak@suiside.cz>
2019-02-21 10:07:16 +0100

    Register form

--- app/presenters/RegistracePresenter.php
+++ app/presenters/RegistracePresenter.php
@@ -3,15 +3,43 @@
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Application\UI;
 
 final class RegistracePresenter extends BasePresenter
 {
-    public function renderDefault() {
+    protected function createComponentRegistrationForm()
+    {
+        $form = new UI\Form;
+        $form->addText('first_name', 'Jméno:')
+            ->setRequired('Zadejte prosím jméno');
+        $form->addText('last_name', 'Příjmení:')
+            ->setRequired('Zadejte prosím příjmení');
+        $form->addText('email', 'E-mail:')
+        ->setRequired('Zadejte prosím email');
 
+        $form->addTextArea('bio', 'Něco o tobě:');
+
+        $form->addCheckbox('newsletter_barcamp', 'Přeješ si dostávat novinky z Barcampu?');
+        $form->addCheckbox('newsletter_techheaven', 'Přeješ si dostávat novinky z TechHeaven?');
+        $form->addSubmit('register', 'Registrovat');
+        $form->onSuccess[] = [$this, 'registrationFormSucceeded'];
+        return $form;
     }
 
-    public function renderList() {
-
+    public function registrationFormSucceeded(UI\Form $form, \stdClass $values)
+    {
+        $values;
+        $toDb = $values;
+        $toDb = [
+            'full_name' => $values->first_name . " " . $values->last_name,
+            'email' => $values->email,
+            'bio' => $values->bio,
+            'newsletter_barcamp' => $values->newsletter_barcamp,
+            'newsletter_techheaven' => $values->newsletter_barcamp,
+            'ip_address' => $_SERVER['REMOTE_ADDR'],
+            'created_at' => date ("Y-m-d H:i:s", time()),
+            ];
+        $this->db->table('participants')->insert($toDb);
+        $this->redirect('Registrace:success');
     }
 }
