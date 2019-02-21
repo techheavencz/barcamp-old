6f4909fd862fb0aad72f66f426ea859688603648
Jakub Bouček <pan@jakubboucek.cz>
2019-02-21 16:48:42 +0100

    Fix wrong saving data from Registration form

--- app/presenters/RegistracePresenter.php
+++ app/presenters/RegistracePresenter.php
@@ -35,7 +35,7 @@ final class RegistracePresenter extends BasePresenter
             'email' => $values->email,
             'bio' => $values->bio,
             'newsletter_barcamp' => $values->newsletter_barcamp,
-            'newsletter_techheaven' => $values->newsletter_barcamp,
+            'newsletter_techheaven' => $values->newsletter_techheaven,
             'ip_address' => $_SERVER['REMOTE_ADDR'],
             'created_at' => date ("Y-m-d H:i:s", time()),
             ];
