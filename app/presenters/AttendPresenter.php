3b230697794bb945e851cb032fa4941e8fbd1d12
Vojtěch Pšenák <psenak.vojtech@gmail.com>
2019-04-10 13:22:22 +0200

    Attend fix

--- app/presenters/AttendPresenter.php
+++ app/presenters/AttendPresenter.php
@@ -23,14 +23,15 @@ final class AttendPresenter extends BasePresenter
     }
 
     public function renderDefault($id, $hash, $isAttending) {
+        if(is_null($id)) {
+            throw new Nette\Application\BadRequestException();
+        }
         $user = $this->userModel->getById($id);
         if($hash != md5($user->email)) {
             throw new Nette\Application\ForbiddenRequestException();
         }
-        if(!($isAttending == 1 OR $isAttending == 0)) {
-            throw new Nette\Application\ForbiddenRequestException();
-        }
-        $this->userModel->setAttending($id, $isAttending);
+
+        $this->userModel->setAttending($id, (bool) $isAttending);
         $this->flashMessage("Díky za info!");
         $this->redirect("Homepage:default");
 
